858b06a7d855b8b5ffa107e24dddb9d0a58b7b80
t3421 <31800350+t3421@users.noreply.github.com>
2017-11-07 10:20:39 -0600

    add cancel button java

--- Calender-app/app/src/main/java/com/t3421/calenderapp/AddCalenderDate.java
+++ Calender-app/app/src/main/java/com/t3421/calenderapp/AddCalenderDate.java
@@ -2,8 +2,11 @@
 
 package com.t3421.calenderapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddCalenderDate extends AppCompatActivity {
 
@@ -11,5 +14,18 @@ public class AddCalenderDate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_calender_date);
+
+        Button cancelAddDate = (Button) findViewById(R.id.cancel_add_date);
+        cancelAddDate.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                Intent intent = new Intent(AddCalenderDate.this, AddEvent.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
--- Calender-app/app/src/main/java/com/t3421/calenderapp/AddTime.java
+++ Calender-app/app/src/main/java/com/t3421/calenderapp/AddTime.java
@@ -1,7 +1,10 @@
 package com.t3421.calenderapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddTime extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class AddTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_time);
+
+        Button cancelAddTime = (Button) findViewById(R.id.cancel_time);
+        cancelAddTime.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                Intent intent = new Intent(AddTime.this, AddEvent.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
     }
 }
