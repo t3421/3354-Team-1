34b2aa5f00abbe3e03dbe35a60c6973b8fa5d109
t3421 <t3421@yahoo.com>
2017-11-26 15:37:21 -0600

    add Jtest

---
+++ Calender-app/app/src/androidTest/java/com/t3421/calenderapp/AddEventTest.java
@@ -0,0 +1,42 @@
+package com.t3421.calenderapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by t3421 on 11/26/2017.
+ */
+public class AddEventTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getDateString() throws Exception {
+
+    }
+
+    @Test
+    public void validate() throws Exception {
+
+    }
+
+    @Test
+    public void getTimeString() throws Exception {
+
+    }
+
+    @Test
+    public void getSpinnerPosition() throws Exception {
+
+    }
+
+    @Test
+    public void setOccurrenceId() throws Exception {
+
+    }
+
+}
