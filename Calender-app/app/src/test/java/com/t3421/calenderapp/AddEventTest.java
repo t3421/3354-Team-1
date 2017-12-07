c4f696dd2da59e24d874600647942a9ea1669a70
t3421 <t3421@yahoo.com>
2017-12-06 22:30:19 -0600

    Fixed test class

--- Calender-app/app/src/test/java/com/t3421/calenderapp/AddEventTest.java
+++ Calender-app/app/src/test/java/com/t3421/calenderapp/AddEventTest.java
@@ -1,55 +1,51 @@
 package com.t3421.calenderapp;
 
+import com.t3421.calenderapp.AddEvent;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
 
 import static org.junit.Assert.*;
 
-
+/**
+ * @author Theodore Sosnowski
+ */
+@RunWith(Parameterized.class)
 public class AddEventTest {
 
-     AddEvent tester;
+    AddEvent tester;
+    int yearInput;
+    int monthInput;
+    int dayInput;
+    String expectedOutput;
+
+    public AddEventTest(String output, int year, int month, int day) {
+        expectedOutput = output;
+        yearInput = year;
+        monthInput = month;
+        dayInput = day;
+    }
 
     @Before
     public void setUp() throws Exception {
         tester = new AddEvent();
     }
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> valuePairs() {
+        return Arrays.asList(new Object[][] {{"1 January 2017", 2017, 1, 1}, {"31 November 2017", 2017, 11, 31}, {"", 0, 1, 1}, {"", 2017, 1, 0}});
+    }
+
     @Test
     public void getDateString() throws Exception {
-        assertEquals("1 January 2017" , tester.getDateString(2017,1,1));
-        assertEquals("31 November 2017" , tester.getDateString(2017,11,31));
-        assertEquals("" , tester.getDateString(0,1,1));
-        assertEquals("" , tester.getDateString(2017,1,0));
-    }
+        assertEquals(expectedOutput, tester.getDateString(yearInput, monthInput, dayInput));
 
-
-    @Test
-    public void validate() throws Exception {
-        assertEquals(true , tester.validate(0,0,12,30));
-        assertEquals(true , tester.validate(24,0,24,1));
-        assertEquals(true , tester.validate(12,30,0,0));
-        assertEquals(false , tester.validate(12,0,11,1));
-    }
-
-    @Test
-    public void getTimeString() throws Exception {
-        assertEquals("" , tester.getTimeString(0,0));
-        assertEquals("12:00 PM" , tester.getTimeString(12,0));
-        assertEquals("12:30 AM" , tester.getTimeString(0,30));
-        assertEquals("9:01 PM" , tester.getTimeString(21,1));
-    }
-
-    @Test
-    public void getSpinnerPosition() throws Exception {
-        assertEquals( 0 , tester.getSpinnerPosition(""));
-        assertEquals( 0 , tester.getSpinnerPosition("Yellow"));
-        assertEquals( 1 , tester.getSpinnerPosition("Fuchsia"));
-        assertEquals( 2 , tester.getSpinnerPosition("Red"));
-        assertEquals( 3 , tester.getSpinnerPosition("Green"));
-        assertEquals( 4 , tester.getSpinnerPosition("Purple"));
-        assertEquals( 5 , tester.getSpinnerPosition("Blue"));
-        assertEquals( 6 , tester.getSpinnerPosition("Maroon"));
-        assertEquals( 7 , tester.getSpinnerPosition("Teal"));
     }
 }
